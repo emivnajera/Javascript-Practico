dbe319c4c90cb89d0cfc4fde45a278016714e9c8
emivnajera <emivelasquez2000@hotmail.com>
2022-11-29 11:17:05 -0600

    addind js queries for html.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hola Mundo");
+
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
+const parrafo = document.querySelector(".parrafo");
+const pid = document.querySelector("#pid");
+const input = document.querySelector("input");
+
+console.log(h1);
+
+console.log(input.value);
+
+console.log({
+  h1,
+  p,
+  parrafo,
+  pid,
+  input,
+});
